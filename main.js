b8b8f5ffa855f416fa2a559b8711954208af1894
MohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-03-30 06:27:25 +0500

    Second commit

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
 #!/usr/bin/env node
-
-import inquirer from "inquirer"
-import chalk from "chalk"
-
+import inquirer from "inquirer";
+import chalk from "chalk";
 const randomNo = Math.floor(Math.random() * 6 + 1);
 const answer = await inquirer.prompt([
-
     {
         name: "userGuessedNo",
         type: "number",
         message: "plz Guess a number ( 1 - 6 ) = ",
-
     }
-
 ]);
-
 if (answer.userGuessedNo === randomNo) {
-    console.log(chalk.green`congratulations ! You Guessed a Right Number .`);
-} else {
-    console.log(chalk.red` You Guessed a Wrong Number `)
+    console.log(chalk.green `congratulations ! You Guessed a Right Number .`);
 }
-
+else {
+    console.log(chalk.red ` You Guessed a Wrong Number `);
+}
 console.log(answer);
-
